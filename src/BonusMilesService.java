b006de97e28d393041eecce200d9156a4b54780a
Alexey <valeskey1988@mail.ru>
2023-02-15 20:50:16 +0500

    change ignore else

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
     public int calculate(int price) {
-    int chek = 20;
-    int bonus = (price / chek);
-    return bonus;
+        int chek = 20;
+        int bonus = (price / chek);
+        return bonus;
     }
 }
